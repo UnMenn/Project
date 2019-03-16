c491290c02a11dfe287aba50cac118aa099d3324
UnMenn <48603877+UnMenn@users.noreply.github.com>
2019-03-15 21:46:24 -0600

    Update omegle_simulator.py

--- omegle_simulator.py
+++ omegle_simulator.py
@@ -1,34 +1,30 @@
 import random
 
-def txt():
-
-   while True:
-    try:
-        age = int(input("age?"))
-
-        if age >= 18:
-            print(age + 2)
-            name1 = ["Jo", "Jam", "Will", "Dav", "Matt", "Rus"]
-            name2 = ["chez", "phy", "ward"]
-            answer2 = str(input("M or F?"))
-            answer1 = str(input("pic?"))
-            randname1 = random.choice(name1)
-            randname2 = random.choice(name2)
-            name = (randname1 + randname2)
-        if answer2 in ["m", "M"]:
-            break
-        if answer2 in ["f", "F"]:
-
-         if answer1 in ["n", "N", "no", "No", "NO"]:
-                break
-        elif answer1 in ["y", "Y", "Yes", "YES"]:
-                print("snap = @" +name)
-                break
-
-        else:
-            break
-    finally:
-        print("bye")
-
-
-txt()
+class omegle():
+    def idk(self):
+        while True:
+            try:
+                age = int(input("age?"))
+                if age >= 18:
+                    print(age + 2)
+                    question2 = str(input("M or F?"))
+                    if question2 in ["m", "M", "male", "Male"]:
+                        break
+                    if question2 in ["f", "F", "female", "Female"]:
+                        answer1 = str(input("pic?"))
+                        if answer1 in ["n", "N", "no", "No", "NO"]:
+                            break
+                if answer1 in ["y", "Y", "Yes", "YES"]:
+                    name1 = ["Jo", "Jam", "Will", "Dav", "Matt", "Rus"]
+                    name2 = ["chez", "phy", "ward"]
+                    randname1 = random.choice(name1)
+                    randname2 = random.choice(name2)
+                    name = (randname1 + randname2)
+                    print("snap = @" +name)
+                    break
+                else:
+                    break
+            finally:
+                print("bye")
+bot = omegle
+bot.idk(1)
